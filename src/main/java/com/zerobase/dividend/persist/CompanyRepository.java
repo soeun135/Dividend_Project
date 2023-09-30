784b4092e4131481d44ed560025b45c0d45ee95b
soeun135 <soensuckun@naver.com>
2023-09-30 18:37:51 +0900

    DB 환경설정, Entity, Repository 구현

---
+++ src/main/java/com/zerobase/dividend/persist/CompanyRepository.java
@@ -0,0 +1,9 @@
+package com.zerobase.dividend.persist;
+
+import com.zerobase.dividend.persist.entity.CompanyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
+}
---
+++ src/main/java/com/zerobase/dividend/persist/DividendRepository.java
@@ -0,0 +1,9 @@
+package com.zerobase.dividend.persist;
+
+import com.zerobase.dividend.persist.entity.DividendEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DividendRepository extends JpaRepository<DividendEntity, Long> {
+}
