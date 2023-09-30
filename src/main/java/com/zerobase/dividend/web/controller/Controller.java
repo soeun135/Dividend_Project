d82591c24cf82e9ba1c22aaa370ce8ea1230b96b
soeun135 <soensuckun@naver.com>
2023-09-30 17:58:51 +0900

    브랜치 변경 test

--- src/main/java/com/zerobase/dividend/web/controller/Controller.java
+++ src/main/java/com/zerobase/dividend/web/controller/Controller.java
@@ -1,6 +1,9 @@
 package com.zerobase.dividend.web.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
