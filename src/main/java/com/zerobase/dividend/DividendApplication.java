a3ea59b668e49b90fb75190fbcd9627b113db65d
soeun135 <soensuckun@naver.com>
2023-10-04 04:00:22 +0900

    레디스 캐시 관련 config Bean생성

--- src/main/java/com/zerobase/dividend/DividendApplication.java
+++ src/main/java/com/zerobase/dividend/DividendApplication.java
@@ -9,12 +9,14 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.io.IOException;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class DividendApplication {
 
 	public static void main(String[] args) {
