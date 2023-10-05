d4dfcbb627b029a2b5e24c7b7c4290db22903227
soeun135 <soensuckun@naver.com>
2023-10-05 23:20:14 +0900

    인증구현 오류발생 - > 미해결

--- src/main/java/com/zerobase/dividend/config/AppConfig.java
+++ src/main/java/com/zerobase/dividend/config/AppConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-
 public class AppConfig {
 
     @Bean
