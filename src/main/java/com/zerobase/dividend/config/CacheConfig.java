b651a5fe169578a1895eebb31ba3a7355d18e453
soeun135 <soensuckun@naver.com>
2023-10-07 18:05:32 +0900

    Redis 설정

--- src/main/java/com/zerobase/dividend/config/CacheConfig.java
+++ src/main/java/com/zerobase/dividend/config/CacheConfig.java
@@ -26,16 +26,14 @@ public class CacheConfig {
     @Bean
     public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
         RedisCacheConfiguration conf = RedisCacheConfiguration.defaultCacheConfig()
-                .serializeKeysWith(
-                        RedisSerializationContext
-                                .SerializationPair
-                                .fromSerializer(
-                                        new StringRedisSerializer()))
-                .serializeValuesWith(
-                        RedisSerializationContext
-                                .SerializationPair
-                                .fromSerializer(
-                                        new GenericJackson2JsonRedisSerializer()));
+                .serializeKeysWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(
+                                new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(
+                                new GenericJackson2JsonRedisSerializer()));
         return RedisCacheManager.RedisCacheManagerBuilder
                 .fromConnectionFactory(redisConnectionFactory)
                 .cacheDefaults(conf)
